caa9c329f7f475ae308dde0da54609adaf9da989
Rajnish132 <78337792+Rajnish132@users.noreply.github.com>
2021-08-22 21:04:30 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,9 +1,8 @@
 (function(window) {
 	var speakWord = "Good Bye";
 	var byeSpeaker = function speak(name) {
- 	if(name.charAt(0) == "J" && name.charAt(0) == "j" )
   		console.log(speakWord + " " + name);
 	}
 
-	window.byeSpeaker = byeSpeaker("Rajnish");
+	window.byeSpeaker = byeSpeaker();
 })(window);
