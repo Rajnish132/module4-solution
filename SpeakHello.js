c7379cee108fc92195007be159c46bd7a79bdde3
Rajnish132 <78337792+Rajnish132@users.noreply.github.com>
2021-08-22 20:48:06 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,1 +1,8 @@
-
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function speak(name) {
+ 	if(name.charAt(0) != "J" && name.charAt(0) != "j" )
+  		console.log(speakWord + " " + name);
+	}
+	window.helloSpeaker = helloSpeaker("Rajnish");
+})(window);
